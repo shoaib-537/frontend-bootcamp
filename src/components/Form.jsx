cf27f1d5a95270d4409cd8ee823c077156e12891
Shoaib M <3445shoaib@gmail.com>
2023-01-07 12:12:26 +0500

    With login form only

---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <div>
+      <form action="">
+        <div className="container">
+          <div className="row">
+            <h1>This is first page</h1>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ src/components/Login.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Typography,
+  Link,
+} from "@mui/material";
+import LockPersonOutlinedIcon from "@mui/icons-material/LockPersonOutlined";
+
+export default function login() {
+  const paperStyle = {
+    height: "70vh",
+    width: 280,
+    padding: 20,
+    margin: "20px auto",
+  };
+  const avatarStyle = {
+    backgroundColor: "green",
+  };
+  const commonStyle = {
+    marginTop: 15,
+    marginBottom: 15,
+  };
+  return (
+    <>
+      <Grid>
+        <Paper elevation={7} style={paperStyle}>
+          <Grid align="center">
+            <Avatar style={avatarStyle}>
+              <LockPersonOutlinedIcon />
+            </Avatar>
+            <h2>Login</h2>
+          </Grid>
+          <TextField
+            variant="standard"
+            label="username"
+            placeholder="Enter your UserName"
+            fullWidth
+            required
+          ></TextField>
+          <TextField
+            variant="standard"
+            label="password"
+            placeholder="Enter your Password"
+            type="password"
+            fullWidth
+            required
+            style={commonStyle}
+          ></TextField>
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="Remember Me"
+          />
+          <Button variant="contained" fullWidth style={commonStyle}>
+            Login
+          </Button>
+          <Typography>
+            <Link href="#">Forget Password</Link>
+          </Typography>
+          <Typography style={commonStyle}>
+            Don't have account?<Link href="#">Create Account</Link>
+          </Typography>
+        </Paper>
+      </Grid>
+    </>
+  );
+}
